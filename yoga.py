45d892ce7b58e50f98d620567347b24665e39a24
MWarynicka <105230011+MWarynicka@users.noreply.github.com>
2022-07-03 11:46:00 +0200

    Update yoga.py

--- yoga.py
+++ yoga.py
@@ -1,8 +1,11 @@
 print("Welcome to Virtual Yoga Class")
 input("Choose your exercise level. Are you beginner, intermediate or advanced?\n")
 input("Would you like to exercises - flow, back pain or relaxation?\n")
-exercise=["Trikonasana", "Bhujangasana", "Janu Sirsasana", "Ardha Matsyendrasana","Adho Mukha Svanasana", "Balasana", "Pascimottanasana", "Vrksasana","Ustrasana", "Viparita Karani Mudra", "Virabhadrasana II", "Virasana", "Upavistha", "Baddha konasana"]
+
+ex_for_beginner=["Trikonasana", "Bhujangasana", "Janu Sirsasana", "Ardha Matsyendrasana","Adho Mukha Svanasana", "Balasana", "Pascimottanasana", "Vrksasana","Ustrasana", "Viparita Karani Mudra", "Virabhadrasana II", "Virasana", "Upavistha", "Baddha konasana"]
+ex_for_intermediate=["Bakasana","Urdhva Dhanurasana","Ardha chandrasana", "Natarajasana"]
+ex_for_advanced=["Sirszasana","Hanumanasana","Sirsasana II","Pincha Mayurasana","Eka Pada Rajakapotasana"]
 
 import random 
-ex=random.choices(exercise, k=10)
+ex=random.choices(ex_for_beginner, k=10)
 print(ex)
