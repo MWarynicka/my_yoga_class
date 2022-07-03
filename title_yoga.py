654f930794e67567207d8706392498c677b7dbbe
MWarynicka <105230011+MWarynicka@users.noreply.github.com>
2022-07-03 14:56:33 +0200

    Add files via upload

---
+++ title_yoga.py
@@ -0,0 +1,26 @@
+
+                                                                                                   
+                                                                                                   
+     YYYYYYY       YYYYYYY     OOOOOOOOO             GGGGGGGGGGGGG               AAA               
+     Y:::::Y       Y:::::Y   OO:::::::::OO        GGG::::::::::::G              A:::A              
+     Y:::::Y       Y:::::Y OO:::::::::::::OO    GG:::::::::::::::G             A:::::A             
+     Y::::::Y     Y::::::YO:::::::OOO:::::::O  G:::::GGGGGGGG::::G            A:::::::A            
+     YYY:::::Y   Y:::::YYYO::::::O   O::::::O G:::::G       GGGGGG           A:::::::::A           
+        Y:::::Y Y:::::Y   O:::::O     O:::::OG:::::G                        A:::::A:::::A          
+         Y:::::Y:::::Y    O:::::O     O:::::OG:::::G                       A:::::A A:::::A         
+          Y:::::::::Y     O:::::O     O:::::OG:::::G    GGGGGGGGGG        A:::::A   A:::::A        
+           Y:::::::Y      O:::::O     O:::::OG:::::G    G::::::::G       A:::::A     A:::::A       
+            Y:::::Y       O:::::O     O:::::OG:::::G    GGGGG::::G      A:::::AAAAAAAAA:::::A      
+            Y:::::Y       O:::::O     O:::::OG:::::G        G::::G     A:::::::::::::::::::::A     
+            Y:::::Y       O::::::O   O::::::O G:::::G       G::::G    A:::::AAAAAAAAAAAAA:::::A    
+            Y:::::Y       O:::::::OOO:::::::O  G:::::GGGGGGGG::::G   A:::::A             A:::::A   
+         YYYY:::::YYYY     OO:::::::::::::OO    GG:::::::::::::::G  A:::::A               A:::::A  
+         Y:::::::::::Y       OO:::::::::OO        GGG::::::GGG:::G A:::::A                 A:::::A 
+         YYYYYYYYYYYYY         OOOOOOOOO             GGGGGG   GGGGAAAAAAA                   AAAAAAA
+                                                                                                   
+                                                                                                   
+                                                                                                   
+                                                                                                   
+                                                                                                   
+                                                                                                   
+                                                                                                   
