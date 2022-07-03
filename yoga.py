d052a21a9d27d4962a62304338938328f1f93741
MWarynicka <105230011+MWarynicka@users.noreply.github.com>
2022-07-03 15:02:04 +0200

    Update yoga.py

--- yoga.py
+++ yoga.py
@@ -2,7 +2,7 @@ from title_yoga.py import introduction
 
 print("Welcome to Virtual Yoga Class")
 input("Choose your exercise level. Are you beginner, intermediate or advanced?\n")
-input("Would you like to exercises - flow, back pain or relaxation?\n")
+input("Would you like to exercises - flow or relaxation?\n")
 
 ex_for_beginner=["Trikonasana", "Bhujangasana", "Janu Sirsasana", "Ardha Matsyendrasana","Adho Mukha Svanasana", "Balasana", "Pascimottanasana", "Vrksasana","Ustrasana", "Viparita Karani Mudra", "Virabhadrasana II", "Virasana", "Upavistha", "Baddha konasana"]
 ex_for_intermediate=["Bakasana","Urdhva Dhanurasana","Ardha chandrasana", "Natarajasana"]
