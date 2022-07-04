86fedd74eb6fc17a4a9249695a87a1216e96ef1d
MWarynicka <105230011+MWarynicka@users.noreply.github.com>
2022-07-04 17:28:54 +0200

    Update yoga.py

--- yoga.py
+++ yoga.py
@@ -10,5 +10,16 @@ ex_for_intermediate=["Bakasana","Urdhva Dhanurasana","Ardha chandrasana", "Natar
 ex_for_advanced=["Sirszasana","Hanumanasana","Sirsasana II","Pincha Mayurasana","Eka Pada Rajakapotasana"]
 
 import random 
-ex=random.choices(ex_for_beginner, k=10)
-print(ex)
+
+advanced_class=[]
+advanced_class += random.choices(ex_for_beginner, k=3)
+advanced_class += random.choices(ex_for_intermediate,k=2)
+advanced_class += random.choices(ex_for_advanced,k=3)
+
+print(', '.join(advanced_class))
+
+intermediate_class=[]
+intermediate_class += random.choices(ex_for_beginner, k=5)
+intermediate_class += random.choices(ex_for_intermediate,k=4)
+
+print(', '.join(intermediate_class))
