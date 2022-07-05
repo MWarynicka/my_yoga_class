a796bb9dd0d37a8ac7eec2d343006e6131b363dd
MWarynicka <mwarynicka@gmail.com>
2022-07-05 20:14:24 +0200

    komentarz dodany

--- yoga.py
+++ yoga.py
@@ -54,7 +54,7 @@ advanced_class += random.choices(ex_for_advanced, k=3)
 intermediate_class=[]
 intermediate_class += random.choices(ex_for_beginner, k=5)
 intermediate_class += random.choices(ex_for_intermediate, k=3)
-
+#another comment
 
 if level == "advanced":
   print(', '.join(advanced_class))
