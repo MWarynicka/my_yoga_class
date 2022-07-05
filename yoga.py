057f50d64ef4d51e2610c3c25d57bbaeb0b6bf0a
MWarynicka <105230011+MWarynicka@users.noreply.github.com>
2022-07-05 08:56:18 +0200

    Update yoga.py

--- yoga.py
+++ yoga.py
@@ -5,6 +5,26 @@ print("Welcome to Virtual Yoga Class")
 input("Choose your exercise level. Are you beginner, intermediate or advanced?\n")
 input("Would you like to exercises - flow or relaxation?\n")
 
+def calculator_bmi():
+  
+  height = float(input("enter your height in m: "))
+  weight =  float(input("enter your weight in kg "))
+  
+  bmi = round(weight / height ** 2)
+   
+  if bmi < 18.5:
+    return print(f"Your BMI is {bmi}, you are underweight.")
+  elif bmi < 25:
+    return  print(f"Your BMI is {bmi}, you have a normal weight.")
+  elif bmi < 30:
+    return  print(f"Your BMI is {bmi}, you are slightly overweight.")
+  elif bmi < 35:
+    return f"Your BMI is {bmi}, you are obese."
+  else:
+    return f"Your BMI is {bmi}, you are clinically obese."
+
+calculator_bmi()
+
 ex_for_beginner=["Trikonasana", "Bhujangasana", "Janu Sirsasana", "Ardha Matsyendrasana","Adho Mukha Svanasana", "Balasana", "Pascimottanasana", "Vrksasana","Ustrasana", "Viparita Karani Mudra", "Virabhadrasana II", "Virasana", "Upavistha", "Baddha konasana"]
 ex_for_intermediate=["Bakasana","Urdhva Dhanurasana","Ardha chandrasana", "Natarajasana"]
 ex_for_advanced=["Sirszasana","Hanumanasana","Sirsasana II","Pincha Mayurasana","Eka Pada Rajakapotasana"]
